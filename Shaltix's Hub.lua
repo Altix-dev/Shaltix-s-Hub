c370b297d406609e91912e684131b62e7954ea3d
Altix-dev <65430336+Altix-dev@users.noreply.github.com>
2020-05-16 02:47:04 +0200

    Shaltix's Hub

---
+++ Shaltix's Hub.lua
@@ -0,0 +1,15 @@
+--[[
+
+--== Shaltix's Hub 1.5 ==--
+[+] Minion Simulator
+[+] Rake Clicker
+[+] Saitama Simulator
+[+] Ramen Simulator
+[+] Parkour!
+[+] Monster Battle
+[+] Farm Land
+
+Discord serveur: https://discord.gg/u7u4zJ7
+--]]
+
+loadstring(game:HttpGet("https://pastebin.com/raw/mLXW7DDY", true))()
